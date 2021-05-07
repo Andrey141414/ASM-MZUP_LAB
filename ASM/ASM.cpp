38a87f62378ec8d20ddbc5a49282bf97de5e5d29
Andrey141414 <andrusha.vinokurov@gmail.com>
2021-05-08 05:19:52 +0700

    MMX

--- ASM/ASM.cpp
+++ ASM/ASM.cpp
@@ -3,43 +3,111 @@
 #include<stdio.h>
 #include <conio.h>
 #include <windows.h>
-
+#include <time.h> // чтобы использовать функцию time()
+#define ROWS 4
+#define COLLUMS 4
 int main()
 {
-    for (int t=0;;)
-    {
+   
+   
+        srand(time(NULL));
+        printf("MMX\n");
+       
+        _int16 MATRIX[ROWS][COLLUMS];//Объявление двумерного массива
+        //Заполнение массива псевдослучайными числами (диапазо [-5;5]))
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLLUMS; j++)
+            {
+                MATRIX[i][j] = -5 + rand() % (5 + 5 + 1);
+            }
+        }
 
-        printf("Line algoritm\n");
-        int a, b, i, y;//Объявление переменных
 
-        //Ввод исхожных данных
-        printf("\nInput a: ");
-        scanf_s("%d", &a);
 
-        printf("\nInput b: ");
-        scanf_s("%d", &b);
-
-        printf("\nInput i: ");
-        scanf_s("%d", &i);
 
         _asm
         {
-
-            mov ecx, [i]    ;// Загрузить I в ecx
-            mov eax, [a]    ;// Загрузить A в eax
-            add eax, [b]    ;// A + B
-            mov ebx, eax    ;// Загрузить A + B в ebx
-            mov eax, 0      ;// Загрузить 0 в ax
-            CYCL : add eax, ecx;// ax + cx
-            loop CYCL       ;
-            imul eax, ebx   ;// (A + B)* (1 + 2 + 3 + … + I)
-            mov[y], eax     ;// Загрузить результат в Y
+        
         }
+        ////массив из нулей
+        //_int16 nill_arr[COLLUMS];
+        //for (int i = 0; i < COLLUMS; i++)
+        //{
+        //    nill_arr[i] = 0;
+        //}
 
-        //Вывод результата
-        printf("\nY = %d", y);
-        _getch();
-        system("cls");
 
-    }
+
+       
+        //_int16 result[COLLUMS];//массив для результата
+        //_int16 PLUS_ELEMENTS[COLLUMS];//массив для счёта положительных переменных в столбцах
+        //_int16 MINUS_ELEMENTS[COLLUMS];//массив для счёта отрицательных в столбцах
+        //_asm
+        //{
+        //    
+        //    lea edx, [MATRIX];//Загразить адрес массива в edx
+        //    mov ecx, ROWS;//Загразить число строк массива в eсx для цикла
+
+        //CYCL: movq mm0, [edx];//поместить строку в mm0
+
+        //    movq mm7, nill_arr;//поместить нулевую строку в mm7 для сравнения
+        //   
+        //    PCMPGTW mm0, mm7;//если выходной не больше входного, то входной заполняется нулями. 
+        //    PSRLW mm0, 15;//побитовый сдвиг на 15 чтобы убрать ненужные биты
+        //    PADDW mm1, mm0;//mm1+mm0
+
+
+        //    movq mm0, [edx];//поместить строку в mm0
+        //    PCMPGTW mm7, mm0;//сравнение элементов, результат в регистр, где была нулевая строка
+        //    PSRLW mm7, 15;//побитовый сдвиг на 15
+        //    PADDW mm3, mm7;//mm3+mm7
+
+
+        //    add edx, (COLLUMS * 2);//перейти на сл строку
+
+        //    loop CYCL;//цикл для подсчёта количества отрицательных и положительных элементов
+
+        //   
+
+        //    movq[PLUS_ELEMENTS], mm1;//положительные
+        //    movq[MINUS_ELEMENTS], mm3; //отрицательные
+        //   
+
+
+
+        //    PCMPEQW mm1, mm3;//сравнение элементов
+        //    //если количество отр и полож элементов одинаково то эл mm1 заполняется единицами)
+        //    PSRLW mm1, 15;//побитовый сдвиг на 15
+        //    movq[result], mm1;//Результаи
+
+        //    EMMS;
+        //}
+
+
+        ////Вывод результата
+        //printf("\n\nMATRIX\n");
+        //for (int i = 0; i < ROWS; i++)
+        //{
+        //    puts("");
+        //    for (int j = 0; j < COLLUMS; j++)
+        //    {
+        //        printf(" %d\t ", MATRIX[i][j]);
+        //    }
+        //}
+
+
+        //printf("\nThe same number of positive and negative elements in\n");
+        //for (int i = 0; i < COLLUMS; i++)
+        //{
+        //    if (result[i])
+        //        printf("%d ", i + 1);
+        //}
+        //printf("\nCOLLUMS\n");
+
+
+        //_getch();
+        //system("cls");
+    
+    
 }
